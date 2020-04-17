d6c27cdef5e4318c5d23e0836b2e9e62da1cb66b
sergio <sergio1alanis@hotmail.com>
2020-04-17 09:18:54 -0500

    agregmos secondInVB.html

---
+++ exercice.js
@@ -0,0 +1,6 @@
+var uno ="var uno string, click aceptar";
+var dos = "var dos numero 23456 da click aceptar";
+const alfa = "constante 10.90 da click aceptar";
+alert(uno);
+alert(dos);
+alert(alfa);
